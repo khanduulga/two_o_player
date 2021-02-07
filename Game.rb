9f7b75523c09aa7ef2a310cf2cb23bd2d0b433c4
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2021-02-07 23:39:39 +0000

    initial commit with planning

---
+++ Game.rb
@@ -0,0 +1,25 @@
+class Game
+  #manages player turns
+  #stores which turn we're on
+  #announces scores
+  #announce game over (a players live finishes)
+  #initialize with 2 players
+end
+
+class Turn
+  #creates a new turn every round
+  #asks a question from current player
+  #announces if answer is correct
+  #initialize with player
+end
+
+class Question
+  #creates a random new question
+  #no need to initialize with anything
+end
+
+
+class Player
+  #stores how many lives player has
+  #initialize with lives
+end
